62927822a245bb74e6d8bd014e3f6582adc29372
Tagoole Mugwa David <tagooledavid4@gmail.com>
2026-05-01 12:47:45 +0300

    Instatiated java project

---
+++ week3/cit-java-week3/src/main/java/com/example/citjavaweek3/CitJavaWeek3Application.java
@@ -0,0 +1,13 @@
+package com.example.citjavaweek3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitJavaWeek3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CitJavaWeek3Application.class, args);
+    }
+
+}
---
+++ week3/cit-java-week3/src/test/java/com/example/citjavaweek3/CitJavaWeek3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.citjavaweek3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitJavaWeek3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
